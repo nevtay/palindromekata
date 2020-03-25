50c5dadd08032096b0234c0b832f2af89190e03a
nevtay <nevintay@gmail.com>
2020-03-25 15:39:57 +0800

    Add line breaks for better visibility

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 function isPalindrome (s) {
   const convertToString = String(s);
+
   const convertToLowerCase = convertToString.toLowerCase();
+
   const removeSpaces = [...convertToLowerCase].filter(char => char !== ' ').join('');
+
   const reverseString = [...removeSpaces].reverse().join('');
+
   const palindromeConfirmed = reverseString === removeSpaces;
+
   return !!(palindromeConfirmed || s === '');
 };
 
